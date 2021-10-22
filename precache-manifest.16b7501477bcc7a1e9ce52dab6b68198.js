563d6447547651e4b50b6f76d71c7618a977f138
chunfen <1070692044@qq.com>
2021-10-22 21:19:08 +0800

    deploy

---
+++ precache-manifest.16b7501477bcc7a1e9ce52dab6b68198.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3662706362405e19fa48",
+    "url": "/Lucky-1/css/app.5cf8c5a5.css"
+  },
+  {
+    "revision": "a8f8d43ecdf7f2960e87589162eb23dc",
+    "url": "/Lucky-1/img/cat.a8f8d43e.png"
+  },
+  {
+    "revision": "d70228ef7b424af0a1e246fdeb7ebc1d",
+    "url": "/Lucky-1/index.html"
+  },
+  {
+    "revision": "3662706362405e19fa48",
+    "url": "/Lucky-1/js/app.983fc194.js"
+  },
+  {
+    "revision": "2d25233dd0c8d944a80f",
+    "url": "/Lucky-1/js/chunk-vendors.46d1db9f.js"
+  },
+  {
+    "revision": "c9f88f47edc9acebd0b83b59b43b6d67",
+    "url": "/Lucky-1/ma.png"
+  },
+  {
+    "revision": "4f29e115fb21aa714e8aeabe626a6fbd",
+    "url": "/Lucky-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Lucky-1/robots.txt"
+  }
+]);
